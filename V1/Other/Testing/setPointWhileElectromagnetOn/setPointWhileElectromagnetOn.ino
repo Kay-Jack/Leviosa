8c80d6a791f3cae0f106f3c7608ecde2086fd4c5
danielMosaic <52857002+wijayad@users.noreply.github.com>
2024-09-11 19:53:54 -0400

    Create testing script to check hall sensor with electromagnets

---
+++ V1/Other/Testing/setPointWhileElectromagnetOn/setPointWhileElectromagnetOn.ino
@@ -0,0 +1,102 @@
+#define IN1 6
+#define IN2 5
+#define IN3 4
+#define IN4 3
+#define ENA 2
+#define ENB 7
+
+#define lv1 500
+
+int eAve_X, eAve_Y;
+
+void setup() {
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  // pinMode(BL,OUTPUT);
+  digitalWrite(IN1,0);
+  digitalWrite(IN2,0);
+  digitalWrite(IN3,0);
+  digitalWrite(IN4,0);
+  analogWrite(ENA,0);
+  analogWrite(ENB,0);
+
+  eAve_X = analogRead(A1);
+  eAve_Y = analogRead(A0);
+
+  int beta = 0.2;
+
+  Serial.begin(31250);
+  Serial.print("Hello, This is program to understand impacts of electromagnet positioning on hall effect readings\r\n");
+
+}
+
+void loop() {
+  // read the value from the sensor:
+
+  digitalWrite(IN3,0);
+  digitalWrite(IN4,0);
+  analogWrite(ENB,0);
+  for (int i = 0; i <= lv1; i++) {
+    Serial.print("Setpoint_X: ");
+    Serial.print(analogRead(A1));
+    // Serial.print("      ");
+    Serial.print("Setpoint_Y: ");
+    Serial.print(analogRead(A0));
+
+    eAve_X = beta * eAve_X + (1 - beta) * analogRead(A1);
+    eAve_Y = beta * eAve_Y + (1 - beta) * analogRead(A0);
+    Serial.print("exp_ave_X: ");
+    Serial.print(eAve_X);
+    Serial.print("exp_ave_Y: ");
+    Serial.print(eAve_Y);
+
+    Serial.print("\r\n");
+    delay(100);
+  }
+
+  digitalWrite(IN3,1);
+  digitalWrite(IN4,0);
+  analogWrite(ENB,1);
+  for (int i = 0; i <= lv1; i++) {
+    Serial.print("Setpoint_X: ");
+    Serial.print(analogRead(A1));
+    // Serial.print("      ");
+    Serial.print("Setpoint_Y: ");
+    Serial.print(analogRead(A0));
+
+    eAve_X = beta * eAve_X + (1 - beta) * analogRead(A1);
+    eAve_Y = beta * eAve_Y + (1 - beta) * analogRead(A0);
+    Serial.print("exp_ave_X: ");
+    Serial.print(eAve_X);
+    Serial.print("exp_ave_Y: ");
+    Serial.print(eAve_Y);
+
+    Serial.print("\r\n");
+    delay(100);
+  }
+  
+  digitalWrite(IN3,0);
+  digitalWrite(IN4,1);
+  analogWrite(ENB,1);
+  for (int i = 0; i <= lv1; i++) {
+    Serial.print("Setpoint_X: ");
+    Serial.print(analogRead(A1));
+    // Serial.print("      ");
+    Serial.print("Setpoint_Y: ");
+    Serial.print(analogRead(A0));
+
+    eAve_X = beta * eAve_X + (1 - beta) * analogRead(A1);
+    eAve_Y = beta * eAve_Y + (1 - beta) * analogRead(A0);
+    Serial.print("exp_ave_X: ");
+    Serial.print(eAve_X);
+    Serial.print("exp_ave_Y: ");
+    Serial.print(eAve_Y);
+
+    Serial.print("\r\n");
+    delay(100);
+  }
+}
