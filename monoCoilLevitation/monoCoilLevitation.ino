5a4265231dbcd3692dc27f010bd87810576f342a
danielMosaic <52857002+wijayad@users.noreply.github.com>
2025-03-23 11:58:36 -0400

    bugfix (variable names)

--- monoCoilLevitation/monoCoilLevitation.ino
+++ monoCoilLevitation/monoCoilLevitation.ino
@@ -6,7 +6,7 @@ int sensorValue = 0;
 int noMagnetPoint = 450;
 int IN2 = 10;
 int EN = 11;
-int set_point2 = set_point - 61;
+int setPoint2 = setPoint - 61;
 
 void setup() {
   Serial.begin(9600);
@@ -19,9 +19,9 @@ void loop()
 {
       sensorValue = analogRead(sensorPin);
       //Serial.println(sensorValue);
-      if(sensorValue >= set_point && sensorValue <= noMagnetPoint){
+      if(sensorValue >= setPoint && sensorValue <= noMagnetPoint){
         digitalWrite(EN, HIGH);
-        while (sensorValue >= set_point2 && sensorValue <= noMagnetPoint){
+        while (sensorValue >= setPoint2 && sensorValue <= noMagnetPoint){
           sensorValue = analogRead(sensorPin);
           //Serial.println(sensorValue);
         }
